b596af9165794b99bb042545f97ee80441a10bcf
Callie Busby <calliebusby@Callies-MacBook-Pro.local>
2021-09-29 21:07:57 -0400

    A story to play the Mad Libs game

---
+++ app.py
@@ -0,0 +1,6 @@
+story = '''There once was a[n] {adjective} goose that lived in {a_place}.
+Everyday, the goose would steal a {noun} and 
+destroy all the {plural_noun} in the convenience store.
+'''
+
+print(story)
